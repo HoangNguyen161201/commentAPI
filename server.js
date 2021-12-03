84367dbd9d846f6d64e81cf5ccad770c0787798d
nguyen quang hoang <hoanghpang@gmail.com>
2021-12-04 02:29:51 +0700

    fff

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     preflightContinue: false,
